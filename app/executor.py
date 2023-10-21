b2ba1c908df50eb36ec30e5f78dcd7203d4eed19
PieceOfGood <31653906+PieceOfGood@users.noreply.github.com>
2023-10-21 09:17:12 +0300

    Update executor.py

--- app/executor.py
+++ app/executor.py
@@ -8,11 +8,16 @@ class Executor:
     def __init__(self, conn: Connection) -> None:
         self.conn = conn
 
-    async def exec(self, func: str, *data: any) -> None:
-        """ Вызывает функцию в UI, передавая ей соответствующие аргументы.
+    async def exec(self, func: str, *data: any) -> any:
+        """ Вызывает функцию в UI, передавая ей соответствующие аргументы и
+        возвращает результат, если функция его так же возвращает.
+        
+        Важно! Функция должна возвращать простой тип, или используйте JSON.
+        
         :param func:     Имя функции, которую нужно вызвать, без скобок.
             Например: "someJavascriptFunctionName", или "console.log"
         :param data:     Любое количество аргументов из простых python-типов.
+        :return:         Возвращает результат вызова функции. 
         """
         # args = ", ".join((encode(d).decode("utf-8") for d in data))
         
@@ -20,4 +25,4 @@ class Executor:
         # ? попыток преобразования `datetime`:
         # ! TypeError: Object of type datetime is not JSON serializable
         args = ", ".join((dumps(d) for d in data))
-        await self.conn.extend.injectJS(f"{func}({args})")
+        return await self.conn.extend.injectJS(f"{func}({args})")
